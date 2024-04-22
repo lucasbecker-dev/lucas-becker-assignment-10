cafa0e5e37b196a69ff1f091e7cf9f35ce834560
Lucas Becker <lucasbecker.dev@gmail.com>
2024-04-22 14:21:30 -0400

    wired in spoonacular urls

--- src/main/java/com/coderscampus/assignment10/controller/MealPlanController.java
+++ src/main/java/com/coderscampus/assignment10/controller/MealPlanController.java
@@ -1,11 +1,19 @@
 package com.coderscampus.assignment10.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class MealPlanController
-{
+@RestController
+public class MealPlanController {
+
+    @Value("${spoonacular.urls.base}")
+    private String baseUrl;
+
+    @Value("${spoonacular.urls.mealplan}")
+    private String mealPlanUrl;
+
+
     @GetMapping("mealplanner/week")
     public ResponseEntity<WeekResponse> getWeekMeals(String numCalories, String diet, String exclusions) {
         // TODO: implement getWeekMeals()
