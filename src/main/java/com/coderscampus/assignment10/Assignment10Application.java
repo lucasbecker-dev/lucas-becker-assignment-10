e5913c6bfedb4cf1e1a758a9c82e3afe5a8670c5
Lucas Becker <lucasbecker.dev@gmail.com>
2024-04-23 11:26:38 -0400

    format

--- src/main/java/com/coderscampus/assignment10/Assignment10Application.java
+++ src/main/java/com/coderscampus/assignment10/Assignment10Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Assignment10Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Assignment10Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment10Application.class, args);
+    }
 
 }
